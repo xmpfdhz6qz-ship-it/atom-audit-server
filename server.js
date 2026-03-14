ff01a681b10ead4183c0123b5954f6942b223f28
xmpfdhz6qz-ship-it <daniel.pokorny3@gmail.com>
2026-03-14 11:58:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get("/audit/:token", (req, res) => {
 
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
