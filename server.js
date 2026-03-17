31ffdbf9c03a0e3775d23739a8f8f399acb8b788
xmpfdhz6qz-ship-it <daniel.pokorny3@gmail.com>
2026-03-17 13:00:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,17 @@ const pool = new Pool({
    ROOT (HOMEPAGE) — GOOGLE VERIFY
 ========================= */
 app.get("/", (req, res) => {
-  res.send("FORCE NEW VERSION 🔥");
-});
+ res.send(`
+  <html>
+    <head>
+      <meta name="google-site-verification" content="kkcofV0qcSDCAwmNaTc4k2mT-NvSE9iXUFwwBC2zZcc" />
+      <title>Atom Foundry</title>
+    </head>
+    <body>
+      <h1>Atom Foundry</h1>
+    </body>
+  </html>
+`);
 
 /* =========================
    FULL REPORT
